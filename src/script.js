f3ebc30c98249f8ef262aa22436ad17d327e313f
mshendie <64661351+mshendie@users.noreply.github.com>
2021-07-08 17:26:21 +0200

    javascript textwriting function on landing page

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
-dotNav("section", "easeOutQuart");
+window.addEventListener("load", function(){
+    let target = document.getElementById("subheading");
+    let text = "God restoring all things to His original intentions in Christ."
+    let loop = false;
+    let speed = 100;
+    let pointer = 0;
+    let timer = setInterval(function(){
+        pointer++;
+        if (pointer <= text.length) {
+            target.innerHTML = text.substring(0, pointer);
+        } else { 
+            if (loop) {pointer = 0;} 
+            else {clearInterval(timer);}
+        }
+    }, speed);
+});

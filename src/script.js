472c95c3b16f80bcde19bc4bf5057f731c163e73
mshendie <64661351+mshendie@users.noreply.github.com>
2021-12-24 17:04:49 +0100

    finally figured out solution for video playing - external link with play icon on hover

--- src/script.js
+++ src/script.js
@@ -15,59 +15,6 @@ window.addEventListener("load", function(){
     }, speed);
 });
 
-function displayVideo() {
-            let video = document.querySelector(".player");
-            if (video.style.display === "none") {
-                video.style.display = "block";
-            } else {
-                video.style.display = "none"
-            }
-}
-        // 2. This code loads the IFrame Player API code asynchronously.
-        var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        // 3. This function creates an <iframe> (and YouTube player)
-        //    after the API code downloads.
-        let videoId = [
-          "KPYshqL1LSw",
-          "hd13_6YgYAk",
-        ];
-        var player;
-        function onYouTubeIframeAPIReady() {
-        player = new YT.Player("player", {
-            height: '430',
-            width: '620',
-            videoId: 'KPYshqL1LSw',
-            playerVars: {
-            playsinline: 1,
-            },
-            // If you want video to play auto. 
-            events: {
-            onReady: onPlayerReady,
-            onStateChange: onPlayerStateChange,
-            }
-        });
-        // 4. The API will call this function when the video player is ready.
-        function onPlayerReady(event) {
-        event.target.playVideo();
-        }
-        // 5. The API calls this function when the player's state changes.
-        //    The function indicates that when playing a video (state=1),
-        //    the player should play for six seconds and then stop.
-        var done = false;
-        function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-            setTimeout(stopVideo, 10000);
-            done = true;
-        }
-        }
-        function stopVideo() {
-        player.stopVideo();
-        };
-    }
-////
 
 function contactForm () {
     let submissionButton = document.getElementById("contact-form-submit");
